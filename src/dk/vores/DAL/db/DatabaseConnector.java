3c4dc33401599d6430d09a435a99ca4ca7fd43ad
EmilseBilse <emil875b@easv365.dk>
2021-04-28 18:08:36 +0200

    DBcon
    lav data mappe + lav database.settings fil. det er kinda vigtigt at når man laver database.settings filen at klikke cancel istedet for ok når den spørger om den skal adde den til git

---
+++ src/dk/vores/DAL/db/DatabaseConnector.java
@@ -0,0 +1,41 @@
+package dk.vores.DAL.db;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+public class DatabaseConnector {
+
+    private static final String PROP_FILE = "data/database.settings";
+    SQLServerDataSource dataSource;
+
+    public DatabaseConnector() {
+        Properties databaseProperties = new Properties();
+        try {
+            databaseProperties.load(new FileInputStream(new File(PROP_FILE)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String server = databaseProperties.getProperty("Server");
+        String database = databaseProperties.getProperty("Database");
+        String user = databaseProperties.getProperty("User");
+        String password = databaseProperties.getProperty("Password");
+
+        dataSource = new SQLServerDataSource();
+        dataSource.setServerName(server);
+        dataSource.setDatabaseName(database);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+    }
+
+    public Connection getConnection() throws SQLServerException {
+        return dataSource.getConnection();
+    }
+}
