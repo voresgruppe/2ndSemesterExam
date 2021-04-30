d2d4ab7b776439bef36429be31af6001919be7ac
ceci6182 <ceci6182@easv365.dk>
2021-04-30 14:06:54 +0200

    LoginView changes

--- src/dk/vores/Main.java
+++ src/dk/vores/Main.java
@@ -16,6 +16,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("gui/loginView/view/loginView.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 }
